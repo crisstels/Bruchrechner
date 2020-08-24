f7547108acf74092ecf2034f288e0814b625606a
Natalie Hasselmann <natalie-hasselmann@web.de>
2020-08-24 18:43:52 +0200

    Initial Commit

--- Program.cs
+++ Program.cs
@@ -7,6 +7,7 @@ namespace Bruchrechner
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Initial Commit");
         }
     }
 }
