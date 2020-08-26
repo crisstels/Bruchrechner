8b8b826c916e1d5f4133426feb93820ae4810d9b
crisstels <natalie-hasselmann@web.de>
2020-08-26 12:17:30 +0200

    test for print

--- Program.cs
+++ Program.cs
@@ -6,8 +6,16 @@ namespace Bruchrechner
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Console.WriteLine("Initial Commit");
+            //Console.WriteLine("Hello World!");
+            //Console.WriteLine("Initial Commit");
+            //Console.ReadKey(true);
+
+            Console.WriteLine("\nWhat is your name? ");
+            var name = Console.ReadLine();
+            var date = DateTime.Now;
+            Console.WriteLine($"\nHello, {name}, on {date:d} at {date:t}!");
+            Console.Write("\nPress any key to exit...");
+            Console.ReadKey(true);
         }
     }
 }
