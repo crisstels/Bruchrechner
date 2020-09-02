54ba02b81add5138a0ef0c6a4855909152e36a24
crisstels <natalie-hasselmann@web.de>
2020-09-02 12:32:17 +0200

    added kürzen  and euklid

--- kuerzen.cs
+++ kuerzen.cs
@@ -4,5 +4,33 @@ namespace Bruchrechner
 {
     partial class Main{
         /* Sucht den ggt, um einen Bruch zu kürzen */
+        public int euklid(int zähler, int nenner)
+        {
+            if (zähler == 0)
+            {
+                return nenner;
+            }
+            else
+            {
+                while (nenner != 0)
+                {
+                    if (zähler > nenner)
+                    {
+                        zähler -= nenner;
+                    }
+                    else
+                    {
+                        nenner -= zähler;
+                    }
+                }
+            }
+            return zähler;
+        }
+        /* Kürzt einen Bruch */
+        public void kürzen(Bruch ergebnis, int ggT)
+        {
+            ergebnis.zähler = ergebnis.zähler / ggT;
+            ergebnis.nenner = ergebnis.nenner / ggT;
+        }
     }
 }
